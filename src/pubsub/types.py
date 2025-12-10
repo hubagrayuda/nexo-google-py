a4c16b6ec92d727f668fbe8a4ca3f42e68c7c1b6
hubagrayuda <hub.agrayuda@gmail.com>
2025-12-10 16:53:13 +0700

    Version 0.0.10
    - refactor: Split publisher and subscriber manager

--- src/pubsub/types.py
+++ src/pubsub/types.py
@@ -1,7 +1,8 @@
+from google.cloud.pubsub_v1 import PublisherClient
 from google.cloud.pubsub_v1.subscriber.message import Message
 from typing import Awaitable, Callable, Concatenate, ParamSpec, TypeVar
 
 
 P = ParamSpec("P")
 R = TypeVar("R", bool, Awaitable[bool])
-MessageController = Callable[Concatenate[str, Message, P], R]
+MessageController = Callable[Concatenate[str, PublisherClient, str, Message, P], R]
